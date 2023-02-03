9e4834aa4c12c1415bf635be8af6039ccf4cceb6
aiddin <arcticdessert@gmail.com>
2023-02-03 16:25:39 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -84,8 +84,9 @@ export default {
 </script>
 
 <style>
-::-webkit-scrollbar { display: none;}
+
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -78,6 +78,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div{
+  overflow: hidden;
+}
 h3 {
   margin: 40px 0 0;
 }
